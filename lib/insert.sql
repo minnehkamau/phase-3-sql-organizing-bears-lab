afa240b38a32b29fb9a0e47da9d699ec167f3229
minnehkamau <minnieches08@gmail.com>
2023-02-22 10:45:41 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Mr. Chocolate', 5, 'M', 'Grey', 'Gentle', 1),
+  ('Rowdy', 3, 'F', 'White', 'Friendly', 1),
+  ('Tabitha', 9, 'M', 'Black', 'Peaceful', 1),
+  ('Sergeant Brown', 10, 'M', 'Grey', 'Witty', 1),
+  ('Melissa', 2, 'M', 'White', 'Wiity', 1),
+  ('Grinch', 13, 'M', 'Brown', 'Gentle', 0),
+  ('Wendy', 5, 'F', 'Black', 'Jovial', 1),
+  (NULL, 10, 'M', 'Grey', 'Friendly', 1);
